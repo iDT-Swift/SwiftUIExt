168d6fb50eb9a61501b8a26c8c32662343ffac56
mshedeed-spree3d <mohamed-shedeed@spree3d.com>
2023-12-05 21:39:52 +0200

    [feat] add proper error handling to font registration methods

--- Sources/SwiftUIExt/FontsView/CustomFonts.swift
+++ Sources/SwiftUIExt/FontsView/CustomFonts.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Gustavo Halperin on 5/19/23.
 //
@@ -9,7 +9,7 @@ import SwiftUI
 
 public
 actor CustomFonts {
-    static public let shared: CustomFonts = .init()
+    public static let shared: CustomFonts = .init()
     private init() {}
     private let Manifest = [
         ("Lato-Regular", "ttf"),
@@ -17,24 +17,43 @@ actor CustomFonts {
         ("Limelight-Regular", "ttf"),
         ("JosefinSans-Medium", "ttf")
     ]
-    nonisolated
-    public var fonst: [(String, String)] { Manifest }
-    public func registerFonts() {
-        Manifest.forEach {
-            registerFont(bundle: .module,
-                         fontName: $0.0,
-                         fontExtension: $0.1)
+    public nonisolated
+    var fonst: [(String, String)] { Manifest }
+    public func registerFonts() throws {
+        do {
+            try Manifest.forEach {
+                try registerFont(bundle: .module,
+                                 fontName: $0.0,
+                                 fontExtension: $0.1)
+            }
+        } catch {
+            throw error
         }
     }
     
-    private func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
-        guard let fontURL = bundle.url(forResource: fontName,
-                                       withExtension: fontExtension),
+    public enum FontRegistrationError: Error {
+        case fontCreationFailed
+        case fontRegistrationFailed(Error)
+    }
+    
+    private func registerFont(bundle: Bundle, fontName: String, fontExtension: String) throws {
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
               let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-              let font = CGFont(fontDataProvider) else {
-                  fatalError("Couldn't create font from data")
+              let font = CGFont(fontDataProvider)
+        else {
+            throw FontRegistrationError.fontCreationFailed
         }
+
         var error: Unmanaged<CFError>?
-        CTFontManagerRegisterGraphicsFont(font, &error)
+        guard CTFontManagerRegisterGraphicsFont(font, &error) else {
+            if let cfError = error?.takeRetainedValue() {
+                // Release error as not to cause a memory leak
+                error?.release()
+                throw FontRegistrationError.fontRegistrationFailed(cfError)
+            } else {
+                error?.release()
+                throw FontRegistrationError.fontRegistrationFailed(NSError(domain: "", code: 0, userInfo: nil) as Error)
+            }
+        }
     }
 }
