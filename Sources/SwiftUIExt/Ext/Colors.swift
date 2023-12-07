12eceafa649cf9bb8b192f16b95beb267ee6454a
mshedeed-spree3d <mohamed-shedeed@spree3d.com>
2023-12-07 19:33:30 +0200

    [feat] add colors to Color extensions

--- Sources/SwiftUIExt/Ext/Colors.swift
+++ Sources/SwiftUIExt/Ext/Colors.swift
@@ -28,6 +28,33 @@ extension Color {
     }
 }
 
+// Protea colors
+extension Color {
+    static let blackPrimaryText = Color.black
+    static let whitePrimaryText = Color.white
+    static let buttonBgDark = Color(hex: "#2B2B2B")
+    static let buttonBgGreen = Color(hex: "#50C878")
+    static let buttonTextColorGreen = Color(hex: "#50C878")
+    static let buttonTextColorRed = Color(hex: "#CC0000")
+    static let dividerColorGreen = Color(hex: "#50C878")
+    static let transparentBackground = Color.clear // For transparent backgrounds
+
+    // Helper to convert hex to Color
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = Double(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
 public
 extension View {
     func colorCaptionErrorForeground(color: Color = Color.CaptionError.foreground) -> some View {
