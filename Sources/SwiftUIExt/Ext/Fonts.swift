e18b0c1edc9c9acfb58e7d57e7e73742e4ce4cab
mshedeed-spree3d <mohamed-shedeed@spree3d.com>
2023-12-13 01:54:36 +0200

    [fix] make new font modifiers public + restore sizeFactor

--- Sources/SwiftUIExt/Ext/Fonts.swift
+++ Sources/SwiftUIExt/Ext/Fonts.swift
@@ -7,6 +7,32 @@
 
 import SwiftUI
 
+/**
+ iPhone Mini Models
+ - iPhone 12 Mini, iPhone 13 Mini: 1080 x 2340 pixels, 476 points per inch (PPI), 5.4-inch screen
+ - Logical Resolution: 360 x 780 points
+ Regular iPhone Models
+ - iPhone 6, 6s, 7, 8, SE (2nd generation): 750 x 1334 pixels, 326 PPI, 4.7-inch screen
+    - Logical Resolution: 375 x 667 points
+ - iPhone X, XS, 11 Pro: 1125 x 2436 pixels, 458 PPI, 5.8-inch screen
+    - Logical Resolution: 375 x 812 points
+ - iPhone XR, 11: 828 x 1792 pixels, 326 PPI, 6.1-inch screen
+    - Logical Resolution: 414 x 896 points
+ - iPhone 12, 12 Pro, 13, 13 Pro: 1170 x 2532 pixels, 460 PPI, 6.1-inch screen
+    - Logical Resolution: 390 x 844 points //TARGET
+ Plus and Max iPhone Models
+ - iPhone 6 Plus, 6s Plus, 7 Plus, 8 Plus: 1080 x 1920 pixels, 401 PPI, 5.5-inch screen
+    - Logical Resolution: 414 x 736 points
+ - iPhone XS Max, 11 Pro Max: 1242 x 2688 pixels, 458 PPI, 6.5-inch screen
+    - Logical Resolution: 414 x 896 points
+ - iPhone 12 Pro Max, 13 Pro Max: 1284 x 2778 pixels, 458 PPI, 6.7-inch screen
+    - Logical Resolution: 428 x 926 points
+ */
+
+public
+let sizeFactor = CGSize(width: UIScreen.main.bounds.size.width / 390,
+                        height: UIScreen.main.bounds.size.height / 844)
+
 struct JosefinSansFontModifier: ViewModifier {
     enum Style: String {
         case regular = "JosefinSans-Regular"
@@ -19,7 +45,7 @@ struct JosefinSansFontModifier: ViewModifier {
     var size: CGFloat
 
     func body(content: Content) -> some View {
-        content.font(.custom(style.rawValue, size: size))
+        content.font(.custom(style.rawValue, size: size * sizeFactor.width))
     }
 }
 
@@ -28,7 +54,7 @@ struct LimelightFontModifier: ViewModifier {
     var size: CGFloat
 
     func body(content: Content) -> some View {
-        content.font(.custom("Limelight-Regular", size: size).weight(weight))
+        content.font(.custom("Limelight-Regular", size: size * sizeFactor.width).weight(weight))
     }
 }
 
@@ -37,7 +63,7 @@ struct SFProFontModifier: ViewModifier {
     var size: CGFloat
 
     func body(content: Content) -> some View {
-        content.font(.system(size: size, weight: weight))
+        content.font(.system(size: size * sizeFactor.width, weight: weight))
     }
 }
 
@@ -45,18 +71,22 @@ struct SFProFontModifier: ViewModifier {
 extension View {
     /**
      Begin Josefin Sans modifiers */
+    public
     func josefinHeaderStyle(size: CGFloat = .title1) -> some View {
         modifier(JosefinSansFontModifier(style: .bold, size: size))
     }
 
+    public
     func josefinBodyStyle(size: CGFloat = .title3) -> some View {
         modifier(JosefinSansFontModifier(style: .regular, size: size))
     }
 
-    func josefinButtonStyle(size: CGFloat = .title3) -> some View {
+    public
+    func josefinButtonStyle(size: CGFloat = .caption1) -> some View {
         modifier(JosefinSansFontModifier(style: .medium, size: size))
     }
 
+    public
     func josefinSubtitleStyle(size: CGFloat = .caption1) -> some View {
         modifier(JosefinSansFontModifier(style: .light, size: size))
     }
@@ -66,7 +96,8 @@ extension View {
 
     /**
      Begin Limelight modifiers */
-    func limelightBodyStyle(size: CGFloat = .doubleLargeTitle) -> some View {
+    public
+    func limelightBodyStyle(size: CGFloat = .largeTitle) -> some View {
         modifier(LimelightFontModifier(weight: .regular, size: size))
     }
 
@@ -75,6 +106,7 @@ extension View {
 
     /**
      Begin SFPro modifiers */
+    public
     func sfProSubtitleStyle(size: CGFloat = .title3) -> some View {
         modifier(SFProFontModifier(weight: .regular, size: size))
     }
