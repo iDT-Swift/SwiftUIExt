69327e852329ff47bf0f56acbdfc3efcd4d5fed4
ideastouch <gustavoh@ideasouch.com>
2023-12-14 19:58:26 -0800

    PRO-68. Added support for multiple instance of NavigationModel and remove support for save path in User Default.
    Temporary the NavigationModel save to user default feature was removed b'cos the call to our backend
    `/v2/garment/spree/collection/protea` stop working when is called automatically.

--- Sources/SwiftUIExt/Models/NavigationModel.swift
+++ Sources/SwiftUIExt/Models/NavigationModel.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import Combine
 
+//TODO: Temporary the save to user default feature was removed b'cos the call to our backend
+//TODO: ... /v2/garment/spree/collection/protea stop working when is called automatically.
 /**
  This class will load from the User Defaults the last NavigationPath used in @path.
  This class will automatically update into the UserDefaults with any change in @path.
@@ -17,18 +19,25 @@ public
 class NavigationModel: ObservableObject {
     @Published public var path: NavigationPath
     public static var shared: NavigationModel = .init()
-    public var pathCount: Int { path.count }
+    static public let sharedId = "shared"
     
     //Private properties
-    static private let pathDataKey = "NavigationModel.pathData"
-    @AppStorage(NavigationModel.pathDataKey)
-    private var pathData: Data?
-    private var cancelables: Set<AnyCancellable> = .init()
+    static private let basePathDataKey = "NavigationModel.pathData."
+    let pathDataKey: String
+    //private var cancelables: Set<AnyCancellable> = .init()
     
-    private init() {
-        path = NavigationPath()
-        guard let data = self.pathData else {
+    public
+    init(id:String = NavigationModel.sharedId) {
+        self.path = NavigationPath()
+        
+        self.pathDataKey = Self.basePathDataKey + id
+        /*
+        defer {
             self.$path.sink(receiveValue: save).store(in: &cancelables)
+        }
+        self.pathDataKey = Self.basePathDataKey + id
+        guard let data = UserDefaults.standard.data(forKey: self.pathDataKey) else {
+            self.path = .init()
             return
         }
         do {
@@ -36,30 +45,40 @@ class NavigationModel: ObservableObject {
                 NavigationPath.CodableRepresentation.self,
                 from: data)
             self.path = NavigationPath(representation)
-            self.$path.sink(receiveValue: save).store(in: &cancelables)
+            print("path set")
         } catch {
             self.path = NavigationPath()
-            self.$path.sink(receiveValue: save).store(in: &cancelables)
+            //TODO: Log something instead of assert as soon as we have a log system implemented
+            assertionFailure("Data decode failed")
         }
+        */
     }
-    public func reset() {
+    public func pathRemoveAll() {
         path.removeLast(path.count)
     }
-    
-    private func save(_ path: NavigationPath) {
-        guard path.count > 0 else {
-            self.pathData = nil
+    @available(*, deprecated, 
+                renamed: "pathRemoveAll()",
+                message: "This function will be removed in version 7.0.0")
+    public func reset() {
+        self.pathRemoveAll()
+    }
+    /*
+    private func save(_ newPath: NavigationPath) {
+        guard newPath.count > 0 else {
+            UserDefaults.standard.removeObject(forKey: self.pathDataKey)
             return
         }
-        guard let representation = path.codable else { return }
+        guard let representation = newPath.codable else { return }
         do {
             let encoder = JSONEncoder()
             let data = try encoder.encode(representation)
-            self.pathData = data
+            UserDefaults.standard.setValue(data, forKey: self.pathDataKey)
         } catch {
+            //TODO: Log something instead of assert as soon as we have a log system implemented
             assertionFailure("Somehow the encoder failed so we clean the whole path")
             // We remove all the values from parh and this should trigger a new call to save.
-            self.path.removeLast(self.path.count)
+            self.pathRemoveAll()
         }
     }
+    */
 }
