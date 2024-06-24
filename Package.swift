83f1ab34a39e6885c5ebec1ff597adb5cc7c1f8b
ideastouch <gustavoh@ideastouch.com>
2024-06-24 01:42:45 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftUIExt",
     platforms: [
         .iOS(.v16),
-        .macOS(.v11_00)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
